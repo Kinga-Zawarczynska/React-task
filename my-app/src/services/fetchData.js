5274488925dafbdead945339ef503298a42e33f1
Kinga <zawarczynska.ko@gmail.com>
2019-12-30 12:50:06 +0200

    deleted else from fetching data

--- my-app/src/services/fetchData.js
+++ my-app/src/services/fetchData.js
@@ -1,13 +1,11 @@
 
 
 export const fetchAllData = () => {
-    return fetch("https://my12.digitalexperience.ibm.com/api/859f2008-a40a-4b92-afd0-24bb44d10124/delivery/v1/content/fa9519d5-0363-4b8d-8e1f-627d802c08a8")
+    return fetch("https://my12.digitalexperience.ibm.com/api/859f2008-a40a-4b92-afd0-24bb44d10124/delivery/v1/content/fa9519d5-0363-4b8d-8e1f-627d802c08a")
     .then(response => {
         if (response.ok) {
             return response.json();
-        } else{
-            return null
         }
     })
-    // .catch(error => error)
+    .catch(error => console.log(error))
 }
